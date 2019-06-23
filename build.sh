6964b1d851dccbbe201967d3d7fd0a73f21025b4
Thomas Pressnell <thomas.pressnell@bskyb.com>
2019-06-23 20:42:45 +0100

    build

--- build.sh
+++ build.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-monogen -L terraform -c monolithe.ini -f ~/git/vsd-api-specifications
-rm -r nuagenetworks
-mv terraform/nuagenetworks ./
-rm -r terraform
-go build
-cp terraform-provider-nuagenetworks ~/.terraform.d/plugins/
+
+while read tag; do
+  echo "processing $tag"
+  rm -rf ./${tag:1}
+
+  API_VERSION=${tag:1} monogen -L terraform -c monolithe.ini -f ~/git/vsd-api-specifications -b $tag
+  if [ $? -eq 0 ]; then
+    mkdir ${tag:1}
+    mv terraform/nuagenetworks ${tag:1}/
+    sed -i "s/terraform-providers\\/terraform-provider-nuagenetworks\\/nuagenetworks/tpretz\\/terraform-provider-nuagenetworks\\/${tag:1}\\/nuagenetworks/" terraform/main.go
+    mv terraform/main.go ${tag:1}/
+    cd ${tag:1}
+    go build -o ../build/terraform-provider-nuagenetworks-${tag:1}
+    cd ..
+  fi
+  rm -r terraform
+done < <(git -C ~/git/vsd-api-specifications tag | grep '^r')
+
